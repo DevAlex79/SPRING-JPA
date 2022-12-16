f88033a3e8acd475d10db77b2e0b6f9beafe3a4d
Alexandra MASI <masialexandra@gmail.com>
2022-12-16 16:04:33 +0100

    Exercice Pile et Exercice 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -44,6 +44,20 @@ public class Main {
         }
         ps.envoi();
 
+        // Exercice Pile
+
+        Pile<String> produitPile = new Pile<String>(String[].class, 1);
+
+        Pile<Produit> produitPile2 = new Pile<Produit>(Produit[].class, 1);
+
+        System.out.println("#############   Exercice 2    #############");
+        System.out.println("Tous les produits");
+        ps.begin();
+        for(Produit produit : ps.findAll()){
+            System.out.println(produit.getId()+" , "+produit.getMarque()+" , "+produit.getReference()+" , "+produit.getDateAchat()+" , "+produit.getPrix());
+        }
+        ps.envoi();
+
         ps.close();
 
     }
