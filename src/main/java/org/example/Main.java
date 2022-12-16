d89e6d545ae3f263f3aebfc8aeff1800ac9901ab
Alexandra MASI <masialexandra@gmail.com>
2022-12-16 16:53:04 +0100

    "ajout produitpascher"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -58,7 +58,14 @@ public class Main {
         }
         ps.envoi();
 
+        System.out.println("Tous les produits au-dessus de 2100 euros : ");
+        ps.begin();
+        for (Produit produitpascher : ps.filterByPrice(2100)) {
+            System.out.println(produitpascher.getId()+" , "+produitpascher.getMarque()+" , "+produitpascher.getReference()+" , "+produitpascher.getDateAchat()+" , "+produitpascher.getPrix());
+        }
+
         ps.close();
+
 
     }
 }
